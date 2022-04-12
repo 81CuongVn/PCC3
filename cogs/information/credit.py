ee8314be770506efa2575f16b7ffb3c2b7c69896
Yui <mattmeiz99@gmail.com>
2022-04-12 21:51:24 +0200

    im stupid... sowwy

--- cogs/information/credit.py
+++ cogs/information/credit.py
@@ -9,7 +9,6 @@ class credit(commands.Cog):
 
     @commands.command(name="credits")
     async def credit(self, ctx):
-       async def credit_slash(self, ctx):
         embed = discord.Embed(title="Credits", color=13565696)
         embed.add_field(name="Bot Owner/Developer", value="<@695229647021015040>", inline=False)
         embed.add_field(name="Bot Developer", value="<@713696771188195368>\n<@443769343138856961>", inline=False)
