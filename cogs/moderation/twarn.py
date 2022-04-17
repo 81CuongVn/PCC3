482e1248b8a08b52c5e7c390c31e5e8ce160564e
YES [German] <53976729+YES-German@users.noreply.github.com>
2022-04-17 18:42:34 +0200

    Add files via upload

---
+++ cogs/moderation/twarn.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+from discord import Option
+import json
+from discord.commands import permissions
+
+class twarn(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+    @commands.command(name="twarn", description="For Moderation")
+    async def twarn(self, ctx, member: discord.Member):
+
+        if member==None:
+            await ctx.send("Please use `,twarn @member`")
+            return
+
+        category = discord.utils.get(ctx.author.guild.categories, name="TICKETS")
+        role = discord.utils.get(ctx.guild.roles, name="Helper")
+
+        if ctx.channel.category == category:
+
+            if role in ctx.author.roles:
+                
+                reason = "Useless ticket"
+
+                await self.new_warn_member(member)
+                await self.update_warns(member, reason)
+
+                await ctx.send(f"Warned {member.mention} for {reason}")   
+
+            else:
+                await ctx.send("You don't have the permissions to use this comman")    
+
+        else:
+            await ctx.send("You can't use this command here")        
+
+
+    
+
+    async def get_warns(self):
+        with open("json_files/warns.json", "r") as f:
+                warns = json.load(f)
+        return warns
+
+    async def new_warn_member(self, member):
+
+            warns = await self.get_warns()
+
+            if str(member.id) in warns:
+                return False
+            else:
+                warns[str(member.id)] = {}
+                warns[str(member.id)]["warn_count"] = 0
+                warns[str(member.id)]["mute_count"] = 0
+                warns[str(member.id)]["ban_count"] = 0
+                warns[str(member.id)]["kick_count"] = 0
+
+            with open("json_files/warns.json", "w") as f:
+                json.dump(warns,f)
+            return True     
+
+    async def update_warns(self, member, reason):
+
+        warns = await self.get_warns()
+
+        warn_count_old = warns[str(member.id)]["warn_count"]
+        warn_count_new = warn_count_old + 1
+        warns[str(member.id)]["warn_count"] = warn_count_new
+        warns[str(member.id)][f"warn {warn_count_new}"] = reason    
+
+        with open("json_files/warns.json", "w") as f:
+            json.dump(warns,f)      
+
+
+def setup(client):
+    client.add_cog(twarn(client)) 
