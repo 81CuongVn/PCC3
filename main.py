8e5c41e6bf55857b38559ec8cc957ab9a8dd1fbe
Yui <mattmeiz99@gmail.com>
2022-04-20 21:57:16 +0200

    Release 1.8.2

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ for directory in os.listdir('./cogs'):
             if filename.endswith(".py"):
                 if filename != 'importantfunctions.py':
                     initial_extensions.append("cogs." + directory + '.' + filename[:-3])
-                    print(filename[:-3] + ' was loaded successfully')
+                    print(directory + "/" + filename[:-3] + " was loaded successfully")
 
 if __name__ == '__main__':
     for extension in initial_extensions:
