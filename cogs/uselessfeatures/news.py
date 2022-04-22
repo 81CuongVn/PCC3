3028cb5da300bb87ea532f3f7b5d416957ded779
YES-German <lololomhi@gmail.com>
2022-04-22 22:20:24 +0200

    Added google to version 1.9.1

--- cogs/uselessfeatures/news.py
+++ cogs/uselessfeatures/news.py
@@ -14,7 +14,8 @@ class botnews_command(commands.Cog):
         embed=discord.Embed(title="Update 1.9.1", description="What is new?", color=13565696)
         embed.set_author(name=f"{ctx.author.name}", url=f"https://discordapp.com/users/{ctx.author.id}", icon_url=f"{ctx.author.avatar.url}")
         embed.add_field(name="FAQ", value="We decided to add a FAQ-System so that we can answer your PCC dedicated questions. <:pog:813886729235988541>\nTo use it, just type `,faq_ask` or `/faq_ask`", inline=False)
-        embed.add_field(name="Bug fixes", value="We fixed no bugs... ¯\\\\\_(ツ)\\_\\/¯ But we made sure, that this release is mostly \"bug free\" again.", inline=False)
+        embed.add_field(name="Google", value="Now you can use `,google <what you want to search>` or `/google` to google some things\nIt's against the rules to google things like p*rn<:hahaha6:740166145167982623>")
+        embed.add_field(name="Bug fixes", value="We fixed some bugs again, and made sure, that this release is mostly \"bug free\".", inline=False)
         embed.set_footer(text="Stay tuned for the next update. 😉")
         await ctx.send(embed=embed)
 
